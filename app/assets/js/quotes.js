7638292a2c620438461c56d6c330b063d1305ee6
Johnathan Lyman <jelyman@gmail.com>
2017-12-28 12:12:53 -0800

    more changes

--- app/assets/js/quotes.js
+++ app/assets/js/quotes.js
@@ -4,16 +4,6 @@ var paginationLines = [
   'If you thought those posts were great, try these!',
   'The next page is where the wild things are... and the next set of posts.'
   ];
-var sidebarQuotes = [
-  "It's everything your mother warned you about.",
-  "Brought to you by a sneeze and a fart.",
-  "Powered by the souls of undocumented codes.",
-  "Make the independent web great again.",
-  "No Ragrets.",
-  "These violent delights have violent ends and in their triumph die."
-  ];
 var randomPaginationLines = Math.floor(Math.random()*paginationLines.length);
-var randomPaginationLines = Math.floor(Math.random()*sidebarQuotes.length);
 
 document.getElementById('hey-look').innerHTML = paginationLines[randomPaginationLines];
-document.getElementById('sidebar-quote').innerHTML = sidebarQuotes[randomPaginationLines];
